0471a9b96de0d9283b5e15212363a511cec35e44
QA-Admin <sarasiraj2009@gmail.com>
2020-03-10 16:35:43 +0000

    Falsk Again

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,15 @@
 # import Flask class from the flask module
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+
 # create a new instance of Flask and store it in app 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+
 # import the ./application/routes.py file
 from application import routes
+
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,10 @@
 # import render_template function from the flask module
-from flask import render_template
+from flask import render_template, redirect, url_for
 # import the app object from the ./application/__init__.py
-from application import app
+from application import app, db
+from application.models import Posts
+from application.forms import PostForm
+
 blogData = [
     {  
         "name": {"first":"John", "last":"Doe"},
@@ -18,7 +21,8 @@ blogData = [
 @app.route('/')
 @app.route('/home')
 def home():
- return render_template('home.html', title='Home', posts=blogData)
+    postData = Posts.query.first()
+    return render_template('home.html', title='Home', post=postData)
 
 @app.route('/about')
 def about():
@@ -28,3 +32,23 @@ def about():
 def login():
  return render_template('login.html', title='Login')
 
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
